3f4fcaece2be70aa274210af80e4fb325ccf689b
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-01-25 18:01:01 +0100

    Add tests for scattered steps

---
+++ tests/data/echo.scattered.cwl
@@ -0,0 +1,26 @@
+#!/usr/bin/env cwl-runner
+cwlVersion: v1.0
+class: Workflow
+
+requirements:
+- class: ScatterFeatureRequirement
+
+inputs:
+  wfmessages: string[]
+
+outputs:
+  out_files:
+    outputSource: echo/echoed
+    type:
+      items: File
+      type: array
+
+steps:
+  echo:
+    run: ../tools/echo.cwl
+    in:
+      message: wfmessages
+    out:
+    - echoed
+    scatter: [message]
+    scatterMethod: nested_crossproduct
--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,3 +1,4 @@
+import pytest
 import os
 import yaml
 
@@ -56,6 +57,24 @@ class TestWorkflowGenerator(object):
 
         assert actual == expected
 
+    def test_save_with_scattered_step(self, tmpdir):
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+        echoed = wf.echo(message=msgs, scatter='message', scatter_method='nested_crossproduct')
+        wf.add_outputs(out_files=echoed)
+
+        wf_filename = tmpdir.join('echo-scattered.cwl').strpath
+        wf.save(wf_filename)
+
+        # make workflows contents relative to tests/data/tools directory
+        actual = load_yaml(wf_filename, os.getcwd() + '/tests/data/tools')
+        expected_wf_filename = 'tests/data/echo.scattered.cwl'
+        expected = load_yaml(expected_wf_filename, '../tools')
+
+        assert actual == expected
+
     def test_add_shebang_to_saved_cwl_file(self, tmpdir):
         wf = WorkflowGenerator()
         wf.load('tests/data/tools')
@@ -72,3 +91,67 @@ class TestWorkflowGenerator(object):
             shebang = f.readline()
 
         assert shebang == '#!/usr/bin/env cwl-runner\n'
+
+
+class TestWorkflowGeneratorWithScatteredStep(object):
+    def test_scatter_method_incorrect(self):
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        with pytest.raises(ValueError):
+            wf.echo(message=msgs, scatter='message', scatter_method='blah')
+
+    def test_scatter_method_correct(self):
+        scatter_methods = ['dotproduct', 'nested_crossproduct',
+                           'flat_crossproduct']
+
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        for method in scatter_methods:
+            echoed = wf.echo(message=msgs, scatter='message', scatter_method=method)
+            assert echoed == 'echo/echoed'
+
+    def test_scatter_variable_incorrect(self):
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        with pytest.raises(ValueError):
+            wf.echo(message=msgs, scatter='incorrect', scatter_method='nested_crossproduct')
+
+    def test_scatter_variable_correct(self):
+        scatter_methods = ['dotproduct', 'nested_crossproduct',
+                           'flat_crossproduct']
+
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        for method in scatter_methods:
+            echoed = wf.echo(message=msgs, scatter='message', scatter_method='nested_crossproduct')
+            assert echoed == 'echo/echoed'
+
+    def test_missing_scatter_argument(self):
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        with pytest.raises(ValueError):
+            wf.echo(message=msgs, scatter_method='nested_crossproduct')
+
+    def test_missing_scatter_method_argument(self):
+        wf = WorkflowGenerator()
+        wf.load('tests/data/tools')
+
+        msgs = wf.add_inputs(wfmessages='string[]')
+
+        with pytest.raises(ValueError):
+            wf.echo(message=msgs, scatter='message')
