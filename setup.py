aa175821ba08be4076252614f55ecd6e4d61bfdb
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2017-02-28 13:25:17 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.0',
+    version='0.3.1',
 
     description='Tool to generate and edit CWL workflows',
     long_description="""Tool to generate and edit workflows in common
